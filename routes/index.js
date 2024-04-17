aa7e28d8b98bedab62fb91c0c6e4d98dbd4aac91
ofer shello <ofer.neto@example.com>
2024-04-17 18:56:54 +0300

    aaa

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // הגדרת ראוטר של הרואט שנגדיר באפ
 router.get("/",async(req,res) => {
-  res.json({msg:"18:55 test"});
+  res.json({msg:"18:57 test"});
 })
 
 // export default
