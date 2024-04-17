c239760778ee0c3b660423d85cc9084dbd0c752f
ofer shello <ofer.neto@example.com>
2024-04-17 19:04:37 +0300

    aaa

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+require("dotenv").config(); 
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/ariel23_ev');
-  console.log("mongo connect idf9 atlas");
+  // await mongoose.connect('mongodb://127.0.0.1:27017/ariel23_ev');
+  await mongoose.connect(process.env.MONGO_CONNECT);
+  console.log("mongo connect arial23 atlas");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
